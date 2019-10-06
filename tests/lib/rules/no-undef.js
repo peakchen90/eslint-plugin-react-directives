50c45b07c945bed17018101382f24ce6e879a66e
陈杰 <chenjie@servyou.com.cn>
2019-10-07 03:43:02 +0800

    no-undef

---
+++ tests/lib/rules/no-undef.js
@@ -0,0 +1,77 @@
+const RuleTester = require('eslint').RuleTester;
+const rule = require('../../../lib/rules/no-undef');
+
+new RuleTester({
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6
+  }
+}).run('no-undef', rule, {
+  valid: [
+    {
+      code: `
+        const list = [1, 2, 3]
+        const foo = <li x-for={(item, index) in list} key={item.id}>{item}</li>  
+      `
+    },
+    {
+      code: `
+        const foo = (
+          <li x-for={(item, index) in [1, 2, 3]} key={item.id}>
+            <span>{item}</span>
+          </li> 
+        ) 
+      `
+    },
+    {
+      code: `
+        const foo = (
+          <li v-for={(item, index) in [1, 2, 3]} key={item.id}>{item}</li> 
+        ) 
+      `,
+      settings: {
+        'react-directives': {
+          prefix: 'v'
+        }
+      }
+    },
+  ],
+  invalid: [
+    {
+      code: `
+        const foo = (
+          <li foo={(item) in [1, 2, 3]} key={item.id}>{item}</li> 
+        ) 
+      `,
+      errors: [
+        { message: /'item' is not defined/ },
+        { message: /'item' is not defined/ },
+        { message: /'item' is not defined/ }
+      ]
+    },
+    {
+      code: `
+        const foo = (
+          <li x-for={item in [1, 2, 3]} key={item.id}>{item}</li> 
+        )
+        item
+      `,
+      errors: [
+        { message: /'item' is not defined/ }
+      ]
+    },
+    {
+      code: `
+        const foo = (
+          <div>
+            <li x-for={item in [1, 2, 3]} key={item.id}>{item}</li>
+            {item} 
+          </div>
+        )
+      `,
+      errors: [
+        { message: /'item' is not defined/ }
+      ]
+    },
+  ]
+});
